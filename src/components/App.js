f702cb0cbda75998a61436d576a881593f74adf2
irfan-007 <thanatos786687@gmail.com>
2024-09-18 23:02:43 +0530

    done 00

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,39 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState, useEffect } from "react";
 
+function App() {
+  const [text, setText] = useState("");
+  const [markDown, setMarkDown] = useState([]);
+
+  useEffect(() => {
+    const addToMarkDown = () => {
+      setMarkDown([...markDown, text]);
+    };
+    addToMarkDown();
+  }, [text]);
+  return (
+    <div
+      style={{
+        display: "grid",
+        height: "100vh",
+        width: "100%",
+        gridTemplateColumns: "1fr 1fr",
+        backgroundColor: "#AAAAAA",
+      }}
+    >
+      <textarea
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+        style={{ padding: "1rem", backgroundColor: "#CCCCCC" }}
+        className="textarea"
+      >
+        #hellooo
+      </textarea>
+      <div style={{ padding: "1rem" }} className="preview">
+        <h1 className="loading">{text}</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
