7357246e374ad6a857bed3909d03dd98979507b8
irfan-007 <thanatos786687@gmail.com>
2024-09-19 13:06:33 +0530

    done 04

--- src/components/App.js
+++ src/components/App.js
@@ -30,7 +30,7 @@ function App() {
       ></textarea>
       <div style={{ padding: "1rem" }} className="preview">
         <h1 className="loading" style={{ fontWeight: "bold" }}>
-          {text.slice(1)}
+          {text}
         </h1>
       </div>
     </div>
